2339c4336bdf1dae1bc55bea2723ca85e47d0cf4
SokolAlexander <asokol1994@gmail.com>
2022-02-25 12:03:38 +0300

    fix-send-events: fix autoplay

--- src/index.tsx
+++ src/index.tsx
@@ -30,9 +30,10 @@ export const Vimeo: React.FC<LayoutProps> = ({
 
   const [autoPlayValue, setAutoPlay] = useState<boolean>(autoPlay)
   const muted = Platform.OS === 'android' && autoPlayValue
-  const toggleAutoPlay = useCallback(() => setAutoPlay(!autoPlayValue), [
-    autoPlayValue,
-  ])
+  const toggleAutoPlay = useCallback(() => {
+    console.log('toggleAutoPlay', autoPlayValue)
+    setAutoPlay(!autoPlayValue)
+  }, [autoPlayValue])
 
   const handlers: any = {}
 
@@ -75,6 +76,8 @@ export const Vimeo: React.FC<LayoutProps> = ({
   }, [onReady])
 
   const registerHandlers = useCallback(() => {
+    console.log(' calling register handlers')
+
     registerBridgeEventHandler('ready', onReadyDefault)
     registerBridgeEventHandler('play', onPlay)
     registerBridgeEventHandler('playProgress', onPlayProgress)
@@ -97,21 +100,25 @@ export const Vimeo: React.FC<LayoutProps> = ({
   }
 
   useEffect(() => {
+    console.log('registering handlers')
     registerHandlers()
   }, [videoId, scalesPageToFit])
 
   const onBridgeMessage = useCallback(
     (event: any) => {
+      console.log('bridgeMessage', event)
       const message = event.nativeEvent.data
       let payload
       try {
         payload = JSON.parse(message)
         if (payload?.name === 'finish') {
-          toggleAutoPlay()
+          // toggleAutoPlay()
         }
       } catch (err) {
         return
       }
+
+      console.log('bridgeMessage', event)
 
       let bridgeMessageHandler = handlers[payload?.name]
       if (bridgeMessageHandler) bridgeMessageHandler(payload?.data)
